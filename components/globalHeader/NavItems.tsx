3824cda374eb959e332bccae80b824e77413b954
Christopher <hej@christopherwilliams.se>
2025-09-10 08:51:23 +0200

    Navigation is responsive.

--- components/globalHeader/NavItems.tsx
+++ components/globalHeader/NavItems.tsx
@@ -11,29 +11,42 @@ const listItemMenu = [
   { label: "Kontakta", href: "/kontakta" },
 ];
 
-const NavItems = () => {
-  const pathname = usePathname();
-  return (
-    <nav>
-      <ul className="h-[50px] flex gap-7 items-center">
-        {listItemMenu.map((listItem, idx) => {
-          const isActive =
-            pathname === listItem.href ||
-            (pathname.startsWith(listItem.href) && listItem.href !== "/");
+type NavItemsProps = {
+  direction?: "row" | "col";
+  onLinkClick?: () => void;
+};
 
-          return (
-            <li
-              key={idx}
-              className={`h-full flex items-center relative ${
-                isActive ? "after:w-full" : ""
-              } after:absolute after:left-0 after:bottom-0 after:h-1 after:bg-gray-900 after:w-0 after:transition-all hover:after:w-full`}
-            >
-              <Link href={listItem.href}>{listItem.label}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+const NavItems = ({ direction = "row", onLinkClick }: NavItemsProps) => {
+  const pathname = usePathname();
+  const isRow = direction === "row";
+  return (
+    <ul
+      className={`flex items-center ${
+        isRow ? "flex-row gap-7 h-[50px]" : "flex-col items-start gap-4 py-4"
+      }`}
+    >
+      {listItemMenu.map((listItem, idx) => {
+        const isActive =
+          pathname === listItem.href ||
+          (pathname.startsWith(listItem.href) && listItem.href !== "/");
+
+        return (
+          <li
+            key={idx}
+            className={`relative h-full flex items-center ${
+              isRow ? "h-full" : ""
+            }
+             ${
+               isActive ? "after:w-full" : ""
+             } after:absolute after:left-0 after:bottom-0 after:h-1 after:bg-gray-900 after:w-0 after:transition-all hover:after:w-full`}
+          >
+            <Link href={listItem.href} onClick={onLinkClick}>
+              {listItem.label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
