5c412a458a073df7fbfb30338b188dc3438e6a98
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-10 11:22:58 +0200

    put interface to to interfaces file

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,1 +1,20 @@
-
+export interface ProductFull {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  tags: string[];
+  reviews: [
+    {
+      rating: number;
+      comment: string;
+      date: Date;
+      reviewerName: string;
+      reviewerEmail: string;
+    }
+  ];
+  images: string[];
+}
