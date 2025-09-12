aad064ded02a12fa6af70d0dd4785f968796bf28
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-12 09:19:23 +0200

    added reviews to the product detail card, todo is to add  fsorting and filtering of reviews

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,45 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ProductFull } from "./interfaces";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function calcDaysSince(dateToCalc: string) {
+  const msPerDay = 1000 * 60 * 60 * 24;
+
+  const previusDate = new Date(dateToCalc);
+  const today = new Date();
+  const diffInMS = today.getTime() - previusDate.getTime();
+  const daysAgo = Math.floor(diffInMS / msPerDay);
+  return daysAgo;
+}
+
+export function descendingReviewRating(reviews: ProductFull["reviews"]) {
+  reviews.sort((first, next) => {
+    console.log(reviews);
+
+    if (first.rating < next.rating) {
+      return 1;
+    }
+    if (first.rating > next.rating) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+export function ascendingReviewRating(reviews: ProductFull["reviews"]) {
+  reviews.sort((first, next) => {
+    console.log(reviews);
+
+    if (first.rating < next.rating) {
+      return 1;
+    }
+    if (first.rating > next.rating) {
+      return -1;
+    }
+    return 0;
+  });
 }
