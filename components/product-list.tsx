5c97f664949a78664ce2d78622f1c982d75bc016
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-13 22:05:05 +0200

    added searchbar, needs, takes boolean for global search

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -10,7 +10,6 @@ export default async function ProductList({
 }) {
   return (
     <div className="flex">
-      ProductList
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
         {productList.map((product, index) => (
           <ProductCard key={index} product={product}></ProductCard>
