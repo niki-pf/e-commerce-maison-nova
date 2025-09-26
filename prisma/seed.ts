62ab8b4090e34b2e92d21602e4bd89ac18a3e27e
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-09-26 17:29:22 +0200

    correct seeding for products and reviews

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,52 +8,42 @@ const products: Prisma.ProductCreateManyInput[] = [];
 const reviews: Prisma.ReviewCreateManyInput[] = [];
 async function allProducts() {
   try {
-    const allProducts = await fetchAllProductsOfMultipleCategories(
-      allCategories
-    );
-    if (allProducts !== undefined) {
-      for (const product of allProducts) {
-        const setProduct: Prisma.ProductCreateInput = {
-          id: product.id,
+    const allProducts =
+      await fetchAllProductsOfMultipleCategories(allCategories);
+
+    if (!allProducts) {
+      throw new Error("error with fetch");
+    }
+    let productIdCounter = 1;
+    for (const product of allProducts) {
+      await prisma.product.create({
+        data: {
           title: product.title,
           description: product.description,
-          category: menCategories.includes(product.category) ? "men" : "female",
-          subCategory: product.category,
+          gender: menCategories.includes(product.category) ? "men" : "female",
+          category: product.category,
           tags: product.tags,
           price: product.price,
           discountPercentage: product.discountPercentage,
           rating: product.rating,
-          stock: product.stock,
-          brand: product.brand,
-          sku: product.sku,
           images: product.images,
           thumbnail: product.thumbnail,
-        };
-        products.push(setProduct);
+        },
+      });
+      if (product.reviews && product.reviews.length > 0) {
+        for (const review of product.reviews) {
+          await prisma.review.create({
+            data: {
+              productId: productIdCounter,
+              rating: review.rating,
+              comment: review.comment,
+              reviewerName: review.reviewerName,
+              reviewerEmail: review.reviewerEmail,
+            },
+          });
+        }
       }
-    }
-  } catch (e) {
-    console.error("feel vid ", e);
-  }
-}
-async function allReviews() {
-  try {
-    const allProducts = await fetchAllProductsOfMultipleCategories(
-      allCategories
-    );
-    if (allProducts !== undefined) {
-      for (const product of allProducts) {
-        product.reviews.map((review) =>
-          reviews.push({
-            productId: product.id,
-            rating: review.rating,
-            comment: review.comment,
-            reviewerName: review.reviewerName,
-            reviewerEmail: review.reviewerEmail,
-          })
-        );
-      }
-      return reviews;
+      productIdCounter++;
     }
   } catch (e) {
     console.error("feel vid ", e);
@@ -63,17 +53,7 @@ async function allReviews() {
 async function main() {
   console.log("start seeding...");
   await allProducts();
-  if (products) {
-    await prisma.product.createMany({
-      data: products,
-    });
-  }
-  await allReviews();
-  if (reviews) {
-    await prisma.review.createMany({
-      data: reviews,
-    });
-  }
+
 }
 
 main()
